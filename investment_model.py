624e7cfb09b26ee8b5da33ea609ee2b6778a4e11
Mark <mark.g.sun@gmail.com>
2020-05-15 16:45:24 -0400

    Commit changes ahead of testing purposes

--- investment_model.py
+++ investment_model.py
@@ -9,7 +9,6 @@ Long-only investment model for medium horizons
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import equity_shared as eq
 
 def split_bin(arr,n_bins):
@@ -21,17 +20,6 @@ def split_bin(arr,n_bins):
     arr_sort['Bin'] = pd.qcut(arr_sort.values.flatten(), n_bins, labels = bin_labels)
     
     return arr_sort
-
-def alpha_mom_str(px_close, ti, t):
-    # Cumulative return
-    px_start = px_close.iloc[ti-t,:]
-    px_end = px_close.iloc[ti,:]
-    alpha = px_end/px_start-1
-    
-    # Remove nan
-    alpha = alpha[~np.isnan(alpha)]    
-    
-    return alpha
 
 def bin_portfolios(px_close, alpha, n_bins):
     # Calculate returns
@@ -65,14 +53,48 @@ def bin_portfolios(px_close, alpha, n_bins):
 if __name__ == '__main__':
     # Pull data
     px_close = eq.pull_hist('Close')
+    t_end, n = px_close.shape
     
-    # Calculate alpha
-    ti = 20
-    t = 20
-    alpha = alpha_mom_str(px_close, ti)  
+    # Calculate returns
+    ret_full = pd.DataFrame(eq.calc_return(px_close), columns = px_close.columns, index = px_close.index[1:])
     
-    # Construct bin portfolios and calculate returns
+    # Lookback and holding periods
+    lookback = 250
+    holding = 400
+    
+    # Portfolio returns
     n_bins = 10
-    ret_bin_cum, alpha_bin = bin_portfolios(px_close.iloc[ti:], alpha, n_bins)
+    ret_bin = pd.DataFrame(np.zeros([t_end-1,n_bins]), index = ret_full.index)
     
+    # For every holding period
+    for t in range(lookback,t_end,holding):
+        # Cumulative return
+        px_start = px_close.iloc[t-lookback,:]
+        px_end = px_close.iloc[t,:]
+        alpha = px_end/px_start-1
+        
+        # Remove nan
+        alpha = alpha[~np.isnan(alpha)]    
+        
+        # Divide into bins
+        alpha_bin = split_bin(alpha, n_bins)
+        
+        # Construct portfolios
+        for b in range(n_bins):
+            # Bin portfolio
+            bin_stocks = alpha_bin[alpha_bin['Bin']==b]
+            
+            # Weights
+            wts = px_close.iloc[0,:].copy()
+            wts[:] = 0
+            wts[bin_stocks.index] = 1/bin_stocks.shape[0]
+            
+            ret_bin.iloc[t:t+holding,b] = np.dot(ret_full.iloc[t:t+holding,:],wts)
+            
+    # Calculate equal weighted index of universe
+    ret_bin['Index'] = ret_full.mean(axis = 1)
+    # Calculate cumulative returns
+    ret_bin_cum = pd.DataFrame(np.cumprod(ret_bin.iloc[lookback:,:]+1, axis = 0)-1, index = ret_full.index)
+    
+    # Plot
     ret_bin_cum.plot()
--- stat_arb_model.py
+++ stat_arb_model.py
@@ -55,11 +55,11 @@ def alpha_vol_wk(data_period):
 # Alpha model
 def alpha_model(t, px_close, px_vol):
     # Calculate individual alphas
-    a_mom = alpha_mom_str(px_close.iloc[t-60:t,:])
+    a_mom = alpha_mom_str(px_close.iloc[t-250:t,:])
     a_vol = alpha_vol_wk(px_vol.iloc[t-5:t,:])
     
     # Aggregate alphas
-    wts = [0,1]
+    wts = [1,0]
     alphas = pd.concat([a_mom,a_vol], axis = 1)*wts/sum(wts)
     alpha = alphas.sum(axis = 1)
     
@@ -70,7 +70,7 @@ def beta_model(px_close):
     # Construct equal-weighted portfolio
     dates = px_close.index[1:]
     labels = px_close.columns
-    ret = pd.DataFrame(calc_return(px_close),index = dates, columns = labels)
+    ret = pd.DataFrame(eq.calc_return(px_close),index = dates, columns = labels)
     ret_eq = ret.mean(axis = 1)
     
     # Create beta series
@@ -89,7 +89,7 @@ def beta_model(px_close):
 # Sigma (covariance) model
 def sigma_model(px_close):
     # Compute returns
-    ret = calc_return(px_close)
+    ret = eq.calc_return(px_close)
     
     # Covariance matrix
     lw = covariance.LedoitWolf().fit(ret)
@@ -132,7 +132,7 @@ def optimize_port(w, beta, sigma, px_close, px_vol, t):
     
     # CVXOPT variables
     P = cvxopt.matrix(sigma)
-    q = cvxopt.matrix(alpha_t)
+    q = cvxopt.matrix(-alpha_t)
     
     # Equality
     A = cvxopt.matrix(np.reshape(beta.values,[1,n]))
